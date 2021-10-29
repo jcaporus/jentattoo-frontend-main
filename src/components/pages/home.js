201d49c471d62e367babfed191d105aa5ea34ca8
John Caporusso <jcaporus@amazon.com>
2021-10-29 11:36:00 -0400

    colors

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export default function Home() {
     return (
         <div className='home-wrapper'>
-            <h1>Welcome to my store!</h1>
+            <h1>Welcome to JenTattoo!</h1>
             <div className="home-buttons-wrapper">
                 <Link to="/items">See all my items</Link>
                 <Link to="/add-item">Add an item</Link>
