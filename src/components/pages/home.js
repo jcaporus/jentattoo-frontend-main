e9d60205c21ea37398e2e78f536feab07e716b6f
John Caporusso <jcaporus@amazon.com>
2021-11-02 21:19:02 -0400

    and more sizing

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -16,7 +16,7 @@ export default function Home() {
             <div className="home-buttons-wrapper">
                 <Link to="/items">See all my items</Link>
                 <Link to="/add-item">Add an item</Link>
-                <Link to="/about">About</Link>
+                <Link to="/about">About JenTattoo</Link>
                 <Link to="/contact">Contact Us</Link>
             </div>
         </div>
